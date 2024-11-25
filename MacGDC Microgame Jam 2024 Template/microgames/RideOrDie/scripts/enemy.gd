56f2f559679b9147b293dfed14998c38de03e6cf
MuizHamzat <muizhamzat@gmail.com>
2024-11-25 09:53:38 -0500

    Enemy now follows player. Deleted globals.gd

--- MacGDC Microgame Jam 2024 Template/microgames/RideOrDie/scripts/enemy.gd
+++ MacGDC Microgame Jam 2024 Template/microgames/RideOrDie/scripts/enemy.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
-
-
+@onready var target = $"../Player"
+var speed = 150
 
 func _physics_process(delta):
-	
-	#print(Globals.player_pos)
+	var direction = (target.position-position).normalized()
+	velocity = direction * speed
+	look_at(target.position) #This makes the enemy actually face the player
 
 	move_and_slide()
