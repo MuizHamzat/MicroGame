5243131a046af0dc4815b4842b64208faceffd17
MuizHamzat <muizhamzat@gmail.com>
2024-11-25 13:10:32 -0500

    Some cleanup (and setup for enemy killing)

--- MacGDC Microgame Jam 2024 Template/microgames/RideOrDie/scripts/enemy.gd
+++ MacGDC Microgame Jam 2024 Template/microgames/RideOrDie/scripts/enemy.gd
@@ -3,9 +3,21 @@ extends CharacterBody2D
 @onready var target = $"../Player"
 var speed = 150
 
+#signal kill
+
 func _physics_process(delta):
 	var direction = (target.position-position).normalized()
 	velocity = direction * speed
 	look_at(target.position) #This makes the enemy actually face the player
+	
+	
 
 	move_and_slide()
+	
+	
+#func _on_body_entered(body):
+	#hide()
+	#kill.emit()
+	#
+	## Must be deferred as we can't change physics properties on a physics callback.
+	#$"../Player".set_deferred("disabled", true)
