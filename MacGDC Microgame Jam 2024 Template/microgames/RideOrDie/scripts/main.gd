27daa350ca3d5e1ed6ad03248d2bbb6a8be59c57
MuizHamzat <muizhamzat@gmail.com>
2024-11-26 19:01:41 -0500

    Enemies now spawn outside of screen

--- MacGDC Microgame Jam 2024 Template/microgames/RideOrDie/scripts/main.gd
+++ MacGDC Microgame Jam 2024 Template/microgames/RideOrDie/scripts/main.gd
@@ -2,8 +2,11 @@ extends Microgame
 
 @onready var spawnTimer = $SpawnTimer
 @onready var player = $Player
+
 var enemy_scene: PackedScene = preload("res://microgames/RideOrDie/scenes/enemy.tscn")
 
+var screen_size = Vector2(640,360)
+var spawn_distance = 50
 var game_over = false
 
 signal game_over_signal
@@ -33,6 +36,20 @@ func _on_player_player_death():
 
 func _on_spawn_timer_timeout() -> void:
 	if not game_over:
+		#Set a random position for the spawned enemy
+		var edge = randi() % 4
+		var spawn_position = Vector2()
+		
+		match edge:
+			0:	#Bottom
+				spawn_position = Vector2(randf()*screen_size.x, screen_size.y+spawn_distance)
+			1:	#Right
+				spawn_position = Vector2(screen_size.x+spawn_distance, randf()*screen_size.y)
+			2: 	#Top
+				spawn_position = Vector2(randf()*screen_size.x, -spawn_distance)
+			3:	#Left
+				spawn_position = Vector2(-spawn_distance, randf()*screen_size.y)
+		
 		#Create an instance of the enemy scene
 		var enemy_instance = enemy_scene.instantiate()
 		#Add that scene as a child of the main scene
@@ -40,12 +57,8 @@ func _on_spawn_timer_timeout() -> void:
 		#Set the enemy's set_player() function to take the player's instance as a parameter
 		enemy_instance.set_player(player)
 		#This will allow the enemy to know of the existence of the player instance
-	
-		#Set a random position for the spawned enemy
-		enemy_instance.global_position = Vector2(
-			randf() * 640,
-			randf() * 360
-		)
+		
+		enemy_instance.global_position = spawn_position
 
 
 func _on_game_over_signal() -> void:
