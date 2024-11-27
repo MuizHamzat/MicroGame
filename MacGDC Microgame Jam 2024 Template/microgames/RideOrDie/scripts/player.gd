a35771e331c5236cd5d76dcbe429168225d47f09
Aidan Heathfield <aidan.heathfield@gmail.com>
2024-11-26 20:14:35 -0500

    added potential invincible mode and made weapon have direction to attacks

--- MacGDC Microgame Jam 2024 Template/microgames/RideOrDie/scripts/player.gd
+++ MacGDC Microgame Jam 2024 Template/microgames/RideOrDie/scripts/player.gd
@@ -1,35 +1,45 @@
 extends CharacterBody2D
 
-@onready var hitbox = $hitbox
+@onready var hitbox = $hitbox/CollisionPolygon2D
+@onready var invincible_mode = $hitbox/CollisionShape2D
 @onready var attackCooldown = $AttackCooldown
 @onready var attackDuration = $AttackDuration
 
 var screen_size
 const SPEED = 300.0
 var game_over = false
+var last_direction = "up"
 
 signal player_death
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	$hitbox/CollisionShape2D.disabled = true
+	hitbox.disabled = true
+	invincible_mode.disabled = true
+	
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
+	setAttackDirection(last_direction)
 	if not game_over:
 		if Input.is_action_pressed("keyboard_down"):
 			velocity.y += 1;
+			last_direction = "down"
 		if Input.is_action_pressed("keyboard_up"):
 			velocity.y -= 1;
+			last_direction = "up"
 		if Input.is_action_pressed("keyboard_left"):
 			velocity.x -= 1;
+			last_direction = "left"
 		if Input.is_action_pressed("keyboard_right"):
 			velocity.x += 1;
-			
+			last_direction = "right"
+
+
 		# Attack
 		if Input.is_action_pressed("keyboard_action") and attackCooldown.is_stopped():
 			#play here animation
-			$hitbox/CollisionShape2D.disabled = false
+			hitbox.disabled = false
 			#Start cooldown and duration timer
 			attackCooldown.start()
 			attackDuration.start()
@@ -55,6 +65,18 @@ func kill(object):
 	object.queue_free()
 
 
+# This sets the hitbox to face up down left or right
+func setAttackDirection(last_direction: String) -> void:
+	match last_direction:
+		"up":
+			hitbox.rotation_degrees = 0
+		"right":
+			hitbox.rotation_degrees = 90
+		"down":
+			hitbox.rotation_degrees = 180
+		"left":
+			hitbox.rotation_degrees = 270
+
 
 func _on_hitbox_body_entered(body):
 	if body.is_in_group("Enemies"):
@@ -62,4 +84,4 @@ func _on_hitbox_body_entered(body):
 
 
 func _on_attack_duration_timeout():
-	$hitbox/CollisionShape2D.set_deferred("disabled", true)
+	hitbox.set_deferred("disabled", true)
